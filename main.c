3fbee070db738214b2729fe2a42cd97c5a7b915b
benzrf <benzrf@benzrf.com>
2017-04-26 12:10:02 -0400

    fancified main.c a bit

--- main.c
+++ main.c
@@ -1,13 +1,46 @@
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "lzw_encode.h"
 #include "lzw_decode.h"
 
+typedef void (*const stage)(int, int);
+
+void pipeline(int in, int out, stage *steps) {
+    for (; *(steps + 1) != NULL; steps++) {
+        int fds[2];
+        pipe(fds);
+        if (fork()) {
+            close(fds[0]);
+            (*steps)(in, fds[1]);
+            close(in);
+            close(fds[1]);
+            return;
+        }
+        close(fds[1]);
+        in = fds[0];
+    }
+    (*steps)(in, out);
+    close(in);
+    close(out);
+}
+
+#define SUB(c, ...) else if (!strcmp(argv[1], #c)) {\
+    const stage p[] = {__VA_ARGS__, NULL};\
+    pipeline(STDIN_FILENO, STDOUT_FILENO, p);\
+}
+#define ERR(code, msg, ...) {\
+    fprintf(stderr, "%s: " msg "\n", argv[0], ##__VA_ARGS__);\
+    return code;\
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 2) return 1;
-    if (argv[1][0] == 'e') lzw_encode(STDIN_FILENO, STDOUT_FILENO);
-    else if (argv[1][0] == 'd') lzw_decode(STDIN_FILENO, STDOUT_FILENO);
-    else return 2;
+    if (argc < 2) ERR(1, "no subcommand given")
+    SUB(compress,   lzw_encode)
+    SUB(decompress, lzw_decode)
+    SUB(id,         lzw_encode, lzw_decode)
+    else ERR(2, "unknown subcommand %s", argv[1])
     return 0;
 }
 
